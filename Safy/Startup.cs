a6494765dcaafb3aab8f2824b1b3188a60f9a5c1
Juan Serna <j.Serna@safened.com>
2019-07-11 16:47:04 +0200

    add cors

--- Safy/Startup.cs
+++ Safy/Startup.cs
@@ -21,6 +21,7 @@ namespace Safy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddScoped<ISpotifyAuthService, SpotifyAuthService>();
@@ -40,6 +41,8 @@ namespace Safy
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseCors("AllowAnyOrigin");
 
             app.UseHttpsRedirection();
             app.UseMvc();
