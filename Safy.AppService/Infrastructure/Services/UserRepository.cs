7744d25562e8ce473f8ee7d90e2b03f8b496a21b
Arnoud Dekker <a.dekker@safened.com>
2019-07-11 13:59:33 +0200

    Added database and test project.

---
+++ Safy.AppService/Infrastructure/Services/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using LiteDB;
+using Safy.AppService.Infrastructure.Contracts;
+using Safy.AppService.Models;
+
+namespace Safy.AppService.Infrastructure.Services
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DbConfig dbConfig;
+
+        public UserRepository(DbConfig dbConfig)
+        {
+            this.dbConfig = dbConfig;
+        }
+
+        public User GetUserById(int id)
+        {
+            using (var db = new LiteDatabase(dbConfig.DbLocation))
+            {
+                var userCollection = db.GetCollection<User>("users");
+                var user = userCollection.FindById(id);
+
+                return user;
+            }
+        }
+
+        public User GetUser(string emailAddress)
+        {
+            using (var db = new LiteDatabase(dbConfig.DbLocation))
+            {
+                var userCollection = db.GetCollection<User>("users");
+                var user = userCollection.FindOne(u => u.EmailAddress == emailAddress);
+
+                return user;
+            }
+        }
+
+        public bool SaveUser(User user)
+        {
+            using (var db = new LiteDatabase(dbConfig.DbLocation))
+            {
+                var userCollection = db.GetCollection<User>("users");
+                userCollection.EnsureIndex(u => u.Id);
+                var success = userCollection.Upsert(user);
+
+                return success;
+            }
+        }
+    }
+}
---
+++ Safy.AppService/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Safy.AppService.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string EmailAddress { get; set; }
+        public string Name { get; set; }
+        public DateTime LastLoginDateTime { get; set; }
+        public DateTime LastActionDateTime { get; set; }
+        public DateTime CreateDateTime { get; set; }
+    }
+}
