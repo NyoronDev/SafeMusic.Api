11b67f7db3f999eab8d0074c7f5b0e941eaccd80
Antal Bokor <a.bokor@safened.com>
2019-07-11 15:39:46 +0200

    Updates

--- Safy.Portal/src/app/music.service.ts
+++ Safy.Portal/src/app/music.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 import { Song } from './song.interface';
 import { Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -15,22 +16,25 @@ export class MusicService {
 
   search(searchText: string): Observable<Song[]> {
     console.log(searchText);
-    return of([
-      {
-        artist: 'Lisandro',
-        name: 'El Boludo',
-        // tslint:disable-next-line:max-line-length
-        image: 'https://cdn.memegenerator.es/imagenes/memes/full/2/63/2635159.jpg',
-        id: '1'
-      },
-      {
-        artist: 'Juan',
-        name: 'Space',
-        // tslint:disable-next-line:max-line-length
-        image: 'https://st2.depositphotos.com/7209366/10104/v/600/depositphotos_101042980-stock-video-europe-from-space-sunrise-earth.jpg',
-        id: '2'
-      },
-    ]);
+
+    return this.httpClient.get<Song[]>(environment.apiUrl + 'search/' + searchText);
+
+    // return of([
+    //   {
+    //     artist: 'Lisandro',
+    //     name: 'El Boludo',
+    //     // tslint:disable-next-line:max-line-length
+    //     image: 'https://cdn.memegenerator.es/imagenes/memes/full/2/63/2635159.jpg',
+    //     id: '1'
+    //   },
+    //   {
+    //     artist: 'Juan',
+    //     name: 'Space',
+    //     // tslint:disable-next-line:max-line-length
+    //     image: 'https://st2.depositphotos.com/7209366/10104/v/600/depositphotos_101042980-stock-video-europe-from-space-sunrise-earth.jpg',
+    //     id: '2'
+    //   },
+    // ]);
   }
 
   addToPlaylist(songId: string) {
