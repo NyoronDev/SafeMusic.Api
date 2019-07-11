1b3f45f0d43f97b9a7f57ad7621cdb0f1880306d
Arnoud Dekker <a.dekker@safened.com>
2019-07-11 15:34:21 +0200

    Added usercontroller.

---
+++ Safy/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Safy.AppService.Infrastructure.Contracts;
+using Safy.AppService.Models;
+
+namespace Safy.Api.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IUserRepository userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public User GetUserById(int id)
+        {
+            var user = userRepository.GetUserById(id);
+
+            return user;
+        }
+    }
+}
