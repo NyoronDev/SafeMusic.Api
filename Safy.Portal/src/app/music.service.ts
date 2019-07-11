56d4f141cc8415cddcdb48b1e63dc9ff882c0972
Antal Bokor <a.bokor@safened.com>
2019-07-11 16:20:47 +0200

    Update

--- Safy.Portal/src/app/music.service.ts
+++ Safy.Portal/src/app/music.service.ts
@@ -18,25 +18,6 @@ export class MusicService {
     console.log(searchText);
 
     return this.httpClient.get<Song[]>(environment.apiUrl + 'search/' + searchText);
-
-    // return of([
-    //   {
-    //     artist: 'Lisandro',
-    //     name: 'El Boludo',
-    //     // tslint:disable-next-line:max-line-length
-    //     image: 'https://cdn.memegenerator.es/imagenes/memes/full/2/63/2635159.jpg',
-    //     album: 'Sos River',
-    //     id: '1'
-    //   },
-    //   {
-    //     artist: 'Juan',
-    //     name: 'Space',
-    //     // tslint:disable-next-line:max-line-length
-    //     image: 'https://st2.depositphotos.com/7209366/10104/v/600/depositphotos_101042980-stock-video-europe-from-space-sunrise-earth.jpg',
-    //     album: 'Andromeda',
-    //     id: '2'
-    //   },
-    // ]);
   }
 
   addToPlaylist(songId: string) {
