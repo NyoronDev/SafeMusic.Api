4e229df5f52732725ef0dcbd8eb67e515275de81
Juan Serna <j.Serna@safened.com>
2019-07-11 15:28:13 +0200

    Add front end

---
+++ Safy.Portal/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MusicService } from './music.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [MusicService],
+  bootstrap: [AppComponent]
+})
+  export class AppModule { }
