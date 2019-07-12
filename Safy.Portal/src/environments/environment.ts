411a417474ab5fa1f6d372e2a41c5d8186650be7
Antal Bokor <a.bokor@safened.com>
2019-07-12 13:13:23 +0200

    mierda

--- Safy.Portal/src/environments/environment.ts
+++ Safy.Portal/src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   // tslint:disable-next-line:max-line-length
   // spotifyUrl: 'https://accounts.spotify.com/authorize?response_type=token&client_id=785665304a7640f399d9aa949b51f331&scope=playlist-modify-public%20playlist-modify-private&redirect_uri=http://localhost:4200/'
   // tslint:disable-next-line:max-line-length
-  spotifyUrl: 'https://accounts.spotify.com/authorize?response_type=token&client_id=785665304a7640f399d9aa949b51f331&scope=playlist-modify-public%20playlist-modify-private%20playlist-read-collaborative%20playlist-read-private&redirect_uri=https://nyorondev.github.io/SafeMusic.Api/?username='
+  spotifyUrl: 'https://accounts.spotify.com/authorize?response_type=token&client_id=785665304a7640f399d9aa949b51f331&scope=playlist-modify-public%20playlist-modify-private%20playlist-read-collaborative%20playlist-read-private&redirect_uri=https://nyorondev.github.io/SafeMusic.Api/'
 };
 
 /*
