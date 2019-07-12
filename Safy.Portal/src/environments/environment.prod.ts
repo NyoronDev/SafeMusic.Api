a9a1359b6b01c5afbf21cb0e8854183d0c4a5b5e
Antal Bokor <a.bokor@safened.com>
2019-07-12 09:42:37 +0200

    Update

--- Safy.Portal/src/environments/environment.prod.ts
+++ Safy.Portal/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   apiUrl: 'https://safenedsoundsystemapi.azurewebsites.net/api/',
   // tslint:disable-next-line:max-line-length
   spotifyUrl : 'https://accounts.spotify.com/authorize?response_type=token&client_id=785665304a7640f399d9aa949b51f331&scope=playlist-modify-public%20playlist-modify-private&redirect_uri=https://nyorondev.github.io/SafeMusic.Api/'
